e9faaac01b5efa4cb8a161d3530d8b4e923835fc
Emre Yoleri <emreyoleri01@gmail.com>
2023-02-03 03:46:52 +0300

    colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const Home = () => {
+  return (
+    <div>
+      <h1 className="bg-orange-400 text-red-100 p-5">Hello world!</h1>
+
+      <h1 className="bg-green-300 text-magenta border-y-zinc-800 border-2 p-5">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        magenta: "#8b658b",
+      },
+    },
   },
   plugins: [],
 };
