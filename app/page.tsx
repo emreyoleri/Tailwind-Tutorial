7df700a924668ee5b2296b464f9495c2d73efccd
Emre Yoleri <emreyoleri01@hotmail.com>
2023-04-13 09:52:55 +0300

    Update app

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
       <h1 className="bg-orange-400 text-red-100 p-5">Hello world!</h1>
 
       <h1 className="bg-green-300 text-magenta border-y-zinc-800 border-2 p-5">
-        Hello world!
+        Hello Main Branch!
       </h1>
     </div>
   );
